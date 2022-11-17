317f5d61cbbc5485f4f263fe9a0fc0b63b041e7b
Anthony Liu <anthony.liu@open.edu.au>
2022-11-17 18:39:20 +1100

    get mock data

---
+++ src/data/get-record-band-festival.tsx
@@ -0,0 +1,42 @@
+interface RecordBand {
+  recordName: string;
+  bandFestivals: BandFestival[];
+}
+
+interface BandFestival {
+  bandName: string;
+  festivals: string[]
+}
+
+// get data for fronted
+export const getRecordBandFestival = (): RecordBand[] => {
+
+  return [
+    {
+      recordName: 'Record Label 1',
+      bandFestivals: [
+        {
+          bandName: 'Band X',
+          festivals: [
+            'Omega Festival',
+          ]
+        },
+        {
+          bandName: 'Band Y',
+          festivals: [],
+        }
+      ]
+    }, {
+      recordName: 'Record Label 2',
+      bandFestivals: [
+        {
+          bandName: 'Band 1',
+          festivals: [
+            'Alpha Festival',
+            'Beta Festival',
+          ],
+        }
+      ]
+    }
+  ];
+}
